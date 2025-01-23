b3663362b34abae654e1dc12b7a8ec0c1afcd7f9
JuanManuelGallego <jmgb8000@gmail.com>
2025-01-23 14:00:39 -0500

    remove cors

--- src/server.js
+++ src/server.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('./db');
-const cors = require('cors');
 const app = express();
 const port = 3000;
 
-app.use(cors());
 app.use(bodyParser.json());
 
 
